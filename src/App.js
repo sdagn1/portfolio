cbcb00c836c3f92c64edaee90d05092ee8b81c51
sdagn1 <samdagnall2@gmail.com>
2023-07-30 18:57:10 +0100

    Added animations for fading in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 /*
   Main things to include:
@@ -25,16 +25,42 @@ import React from 'react';
 
     - Design the CSS for the general topic element
 
+    Also need to include the job title and date range
 
     About:
 
     Hi, I'm currently a student at the University of Warwick etc. etc. (Just basically stuff from CV)
 */
+
+const testTags = ["C#", "Java", "Python"];
+
+const eduBlurb = "Achieved a First Class in all years of study at the university so far. "
+const eduTags =["Java", "C", "Python", "SQL", "Spring"]
+
+const loraxTags = ["C#", "SQL", "AWS", "S3", "CloudWatch", "Azure DevOps"]
+
+
+const mentoTags = ["Spring Boot", "Vaadin"]
+
+const timeTags = ["JavaScript", "Android Studio", "Apache Cordova"]
+
+const tetrisTags = ["Python", "PyTorch", "PyQt5", "TensorBoard"]
+
+class SectionTag extends React.Component{
+  render(){
+    return(
+      <div class="text-white text-3xl my-3 hover:font-bold self-start">
+        {this.props.tag}
+      </div>
+    )
+  }
+
+}
 class SectionTitle extends React.Component{
   render(){
     return(
-      <div>
-
+      <div class="font-bold text-4xl text-left m-5 w-3/5">
+        {this.props.title}
       </div>
 
     )
@@ -43,34 +69,57 @@ class SectionTitle extends React.Component{
 class Tag extends React.Component{
   render(){
     return(
-      <div>
-
+      <div class="rounded-full bg-gray-300 hover:bg-gray-400 items-center p-3 ml-2">
+        {this.props.tag}
       </div>
     );
   }
 }
 
-class Topic extends React.Component{
+function Topic(props){
 
-  render(){ 
+
+    const [isReadMore, setIsReadMore] = useState(false);
+    const toggleReadMore = () => {
+      setIsReadMore(!isReadMore);
+    };
+
     return (
 
-      <div class="border border-solid flex-col border-5 border-black m-5 p-5 w-3/5 rounded-lg shadow-lg">
+      <div class={"border border-solid flex-col border-5 border-black m-5 p-5 w-3/5 rounded-lg shadow-lg transition-all ease-in-out duration-100 " + (isReadMore ? "max-h-96" : "max-h-72" )}>
         
-        <div class="flex-row flex">
+        <div class="flex-row flex pb-10">
 
           <div class="flex-col w-4/5">
-            <div class="text-left text-lg underline">
-              {this.props.title}
+            <div class="text-left underline text-3xl">
+              {props.title}
             </div>
-            <div>
-              Blurb Blurb
+            <div class="text-left italic text-2xl my-2">
+              {props.job}
+            </div>
+            <div className='text-1xl my-2'>
+              {props.blurb}
+            </div>
+            <div class={"text-8xl transition-all ease-in-out duration-100 " + (isReadMore ? "animate-fadeIn" : "animate-fadeOut hidden")} >
+              More more
             </div>
 
 
           </div>
           <div class="justify-self-end w-1/5">
             <img src={require("./images/SamJak.png")} alt="SamJak" width="200"/>
+          </div>
+        </div>
+        <div class="flex-row flex justify-between">
+          <div class="flex-row flex">
+            {props.tags.map(item => ( <Tag tag={item}/>))}
+
+          </div>
+          
+          
+
+          <div class="flex justify-end w-1/5">
+            <img src={require("./images/SamJak.png")} alt="SamJak" width="50" onClick={toggleReadMore}/>
           </div>
         </div>
 
@@ -81,35 +130,46 @@ class Topic extends React.Component{
 
   );
   }
-}
 
 function Portfolio() {
   return (
     <div class="flex flex-row">
-      <div class="bg-green-200 w-1/5 h-screen flex flex-col items-center p-8">
+      <div class="bg-green-200 w-1/5 h-screen flex flex-col items-center p-8 fixed">
+        <div class="overflow-hidden rounded-full border-solid border-white border border-4 mb-5">
+          <img src={require("./images/4115525.png")} alt="SamJak" width="200"/>
+        </div>
+
         <div class="text-white font-bold text-4xl">
           Sam Dagnall
         </div>
         
-        <div>
-          <div>
-            About Me
-          </div>
-          <div>
-            Experience
-          </div>
-          <div>
-            Education
-          </div>
-          <div>
-            Projects
-          </div>
+
+        <div class="w-3/5">
+          <SectionTag tag="About Me" />
+          <SectionTag tag="Experience" />
+          <SectionTag tag="Education" />
+          <SectionTag tag="Projects" />
         </div>
 
       </div>
-      <div class="flex flex-col items-center flex-grow h-screen w-full p-2/5">
-        <Topic title="University of Warwick"/>
-        <Topic title="Lorax EPI"/>
+      <div class="w-1/5 h-screen"/>
+      <div class="flex flex-col items-center flex-grow h-screen">
+        <SectionTitle title="About Me"/>
+        <div class="text-left w-3/5">
+        I am a third year Computer Science student currently studying at the University of Warwick, with a love for problem solving and a desire to gain more experience as a software developer through both extra-curricular projects and professional industry experience. 
+        <br/><br/>
+Currently looking for internship/placement positions for Summer 2023.
+
+        </div>
+        <SectionTitle title="Experience" />
+        <Topic title="Lorax EPI" job="Junior Software Engineer" tags={loraxTags}/>
+        <SectionTitle title="Education" />
+        <Topic title="University of Warwick" blurb={eduBlurb} tags={eduTags}/>
+        <SectionTitle title="Projects"/>
+        <Topic title="Mento" tags={mentoTags}/>
+        <Topic title="Tetris Deep Learning" tags={tetrisTags}/>
+        <Topic title="Smart Timeout" tags={timeTags}/>
+
       </div>
       
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty:{
+        height:'height'
+      },
+      animation: {
+        fadeIn: 'fadeIn ease-in-out 0.25s',
+        fadeOut: 'fadeOut ease-in-out 1s',
+      },
+      keyframes:{
+        fadeIn: {
+          '0%': {transform: 'scaleY(0)', opacity: '0%'},
+          '10%': {transform: 'scaleY(0.2)', opacity: '0%'},
+          '20%': {transform: 'scaleY(0.4)', opacity: '0%'},
+          '30%': {transform: 'scaleY(0.6)', opacity: '0%'},
+          '40%': {transform: 'scaleY(0.8)', opacity: '0%'},
+          '50%': {opacity: '0%', transform: 'scaleY(1)'},
+          '60%': {opacity: '20%'},
+          '70%': {opacity: '40%'},
+          '80%': {opacity: '60%'},
+          '90%': {opacity: '80%'},
+          '100%': {opacity: '100%'}
+        },
+        fadeOut: {
+          '0%': {transform: 'scale(1)', opacity: '100%' },
+          '50%': {transform: 'scale(0.5)', opacity: '50%'},
+          '100%': {transform: 'scale(1)', opacity: '0%'}
+        }
+      }
+    },
   },
   plugins: [],
 }
